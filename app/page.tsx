dae931ac142e3accba151c730f326194bdb04e1a
habil_ <habil.m@bluelogic.ae>
2024-07-22 19:27:59 +0400

    updated phone number

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { useEffect } from "react";
 import Header from "./componets/header";
 import ProductList from "./sections/productsList";
 import Link from "next/link";
@@ -101,7 +101,7 @@ export default function Home() {
                 </h3>
                 <div className="flex flex-col gap-1">
                   <Link
-                    href="tel:+971529703006"
+                    href="tel:+971582084146"
                     className="flex items-center gap-2"
                   >
                     <svg
@@ -112,12 +112,12 @@ export default function Home() {
                       fill="none"
                       stroke="currentColor"
                       strokeWidth="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                     </svg>
-                    +971529703006
+                    +971 58 208 4146
                   </Link>
                   <Link
                     href="mailto:sales@timeland.ae"
@@ -131,8 +131,8 @@ export default function Home() {
                       fill="none"
                       stroke="currentColor"
                       strokeWidth="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <rect width="20" height="16" x="2" y="4" rx="2" />
                       <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
@@ -141,12 +141,12 @@ export default function Home() {
                   </Link>
                 </div>
 
-                <div className="w-full flex items-center py-4 mt-0">
+                <div className="w-full flex items-center py-4 mt-0 gap-2">
                   <Link
                     href="https://www.tiktok.com/@timelandwatchestrading1"
                     rel="nofollow"
                     target="_blank"
-                    className="mx-2"
+                    className=""
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -161,7 +161,7 @@ export default function Home() {
                     href="https://www.instagram.com/timeland_watches_ajman"
                     rel="nofollow"
                     target="_blank"
-                    className="mx-2"
+                    className=""
                   >
                     <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24">
                       <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"></path>
@@ -169,7 +169,7 @@ export default function Home() {
                   </Link>
                   <Link
                     href="https://www.instagram.com/timeland_watches_ajman"
-                    className="mx-2"
+                    className=""
                     rel="nofollow"
                     target="_blank"
                   >
